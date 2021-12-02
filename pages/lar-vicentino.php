181d555dbf99c6a8073b69728c720eb4ca51a014
giovanasilveiraa <giovana.silveira01@etec.sp.gov.br>
2021-12-02 21:09:52 -0200

    ggg

--- pages/lar-vicentino.php
+++ pages/lar-vicentino.php
@@ -19,7 +19,24 @@
 </head>
 
 <body>
-    <div class="container">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand mx-5" href="#">Doar.com</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Instituições</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    <div class="container-div">
         <form action="" method="POST" class="form-control">            
 
             <p>Colabore com o Lar Vicentino</p>
