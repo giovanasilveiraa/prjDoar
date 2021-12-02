9db65635afe055f77fbf2fa25cd8d40ca9b438a7
dreehgoncalves <andressagoncalves1997@gmail.com>
2021-12-02 15:43:35 -0300

    .

--- index.php
+++ index.php
@@ -8,9 +8,11 @@
 
     <!-- css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estiloindex.css">
 
     <!-- JavaScript  -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
 
     <!-- Bootbox -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js">
@@ -22,13 +24,59 @@
 <body>
     <a href="cadastro.php">cad</a>
 </body>
-<!-- <h3>Instituições</h3>
-    <p>Projeto Guri</p>
-    <p>Instituto Luisa Mell</p>
-    <p>Lar Vicentino</p>
-    <p>AMPARA Animal</p>
-    <p>Doutores da Alegria</p>
-    <p>Retiro dos artistas</p>
-    <p>Lar do Amor</p>
-    <p>ACDC</p> -->
+
+<h3>Instituições</h3>
+<div class="container">
+    <div class="wrapper">
+        <div class="box a">
+            <p>Projeto Guri</p>
+            <a href="pages/projeto-guri.php">
+                <img alt="projeto-guri" src="imagens/prjGuri.jpg" width="90%" height="90%">
+            </a>
+        </div>
+        <div class="box b">
+            <p>Instituto Luisa Mell</p>
+            <a href="pages/instituto-luiza-mel.php">
+                <img alt="instituto-luiza-mel" src="imagens/luisaMell.jpg" width="90%" height="90%">
+            </a>
+        </div>
+        <div class="box c">
+            <p>Lar Vicentino</p>
+            <a href="pages/lar-vicentino.php">
+                <img alt="lar-vicentino" src="imagens/larVicentino.jpg" width="90%" height="90%">
+            </a>
+        </div>
+        <div class="box d">
+            <p>AMPARA Animal</p>
+            <a href="pages/ampara-animal.php">
+                <img alt="ampara-animal" src="imagens/amparaAnimal.jpg" width="90%" height="90%">
+            </a>
+        </div>
+        <div class="box e">
+            <p>Doutores da Alegria</p>
+            <a href="pages/doutores-da-alegria.php">
+                <img alt="doutores-da-alegria" src="imagens/doutoresAlegria.jpg" width="90%" height="90%">
+            </a>
+        </div>
+        <div class="box f">
+            <p>Retiro dos artistas</p>
+            <a href="pages/retiro-dos-artistas.php">
+                <img alt="retiro-dos-artistas" src="imagens/Retiro-dos-Artistas.jpg" width="90%" height="90%">
+            </a>
+        </div>
+        <div class="box g">
+            <p>Lar do Amor</p>
+            <a href="pages/lar-do-amor.php">
+                <img alt="lar-do-amor" src="imagens/larDoAmor.jpg" width="90%" height="90%">
+            </a>
+        </div>
+        <div class="box h">
+            <p>ACDC</p>
+            <a href="pages/acdc.php">
+                <img alt="acdc" src="imagens/acdc.png" width="90%" height="90%">
+            </a>
+        </div>
+    </div>
+</div>
+
 </html>
