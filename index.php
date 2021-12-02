cbcab0e0b4842a610705119b743842361f8b99a6
geisacardoso <geisavitoriaferreiracardoso@gmail.com>
2021-12-02 20:00:38 -0300

    g4

--- index.php
+++ index.php
@@ -81,9 +81,8 @@
       <p align="center" class="paragrafo">Instituição Luisa Mell</p>
     </div>
   </div>
-  <div class="div">
-
-  <a href="cadastro.php"><button class="btn btn-dark">Cadastrar</button></a>
-  </div>
+  <div class="d-grid gap-2 col-6 mx-auto">
+  <button class="btn btn-dark" type="button">Cadastrar</button>
+</div>
 </body>
 </html>
