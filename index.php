a9584aae998a138a6489312a5cfad51ba1a7139a
giovanasilveiraa <giovana.silveira01@etec.sp.gov.br>
2021-12-02 12:17:18 -0300

    Merge branch 'main' of https://github.com/giovanasilveiraa/prjDoar into main

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 
     <!-- css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 
     <!-- JavaScript  -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -20,15 +21,60 @@
 </head>
 
 <body>
-    <a href="cadastro.php">cad</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Doar.com</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Instituições</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<h3 class="h3">Escolha uma instituição para doar!</h3>
+    <div class="container-div">
+  <div class="row align-items-center">
+    <div class="col-sm-4">
+      <img class="rounded mx-auto d-block" src="imagens/acdc.png" alt="">
+      <p align="center" class="paragrafo">Instituição ACDC</p>
+    </div>
+    <div class="col-sm-4">
+      <img class="rounded mx-auto d-block" src="imagens/amparaAnimal.jpg" 
+      alt="">
+      <p align="center" class="paragrafo">Instituição Ampara Animal</p>
+    </div>
+    <div class="col-sm-4">
+      <img class="rounded mx-auto d-block" src="imagens/doutoresAlegria.jpg" alt="">
+      <p align="center" class="paragrafo">Instituição Doutores da Alegria</p>
+    </div>
+  </div>
+  <div class="container-div">
+  <div class="row align-items-center">
+    <div class="col-sm-4">
+      <img class="rounded mx-auto d-block" src="imagens/larDoAmor.jpg" alt="">
+      <p align="center" class="paragrafo">Instituição Lar do Amor</p>
+    </div>
+    <div class="col-sm-4">
+      <img class="rounded mx-auto d-block" src="imagens/larVicentino.jpg" 
+      alt="">
+      <p align="center" class="paragrafo">Instituição Lar Vicentino</p>
+    </div>
+    <div class="col-sm-4">
+      <img class="rounded mx-auto d-block" src="imagens/luisaMell.jpg" alt="">
+      <p align="center" class="paragrafo">Instituição Luisa Mell</p>
+    </div>
+  </div>
+  <div class="div">
+  <button type="submit" class="btn btn-dark">Doar</button>
+  <a href="cadastro.php"><button class="btn btn-dark">Cadastrar</button></a>
+  </div>
 </body>
-<!-- <h3>Instituições</h3>
-    <p>Projeto Guri</p>
-    <p>Instituto Luisa Mell</p>
-    <p>Lar Vicentino</p>
-    <p>AMPARA Animal</p>
-    <p>Doutores da Alegria</p>
-    <p>Retiro dos artistas</p>
-    <p>Lar do Amor</p>
-    <p>ACDC</p> -->
 </html>
