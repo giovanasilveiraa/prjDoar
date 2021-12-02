f3cadf869ae8cfab61dea10a4a254d57f45dc2de
geisacardoso <geisavitoriaferreiracardoso@gmail.com>
2021-12-02 19:56:53 -0300

    g3

--- index.php
+++ index.php
@@ -24,16 +24,13 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Doar.com</a>
+    <a class="navbar-brand mx-5" href="#">Doar.com</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Instituições</a>
         </li>
       </ul>
     </div>
