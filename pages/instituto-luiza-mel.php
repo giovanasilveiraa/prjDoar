eb9648c353be493354f69d079b096ed6a30e3eef
Giovana Silveira <giovana.silveira01@etec.sp.gov.br>
2021-12-03 22:25:40 -0300

    Add files via upload
    conclusão

--- pages/instituto-luiza-mel.php
+++ pages/instituto-luiza-mel.php
@@ -1,53 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/estilopaginas.css">
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="../css/estilopaginas.css" />
 
     <!-- JavaScript  -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js">
-    </script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+      integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+      crossorigin="anonymous"
+    ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js"></script>
     <title>Doar.com</title>
-</head>
+  </head>
 
-<body>
+  <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand mx-4" href="#">Doar.com</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
-                    </li>
-
-                </ul>
-            </div>
+      <div class="container-fluid">
+        <a class="navbar-brand mx-4" href="#">Doar.com</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        ></button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="../index.php"
+                >Home</a
+              >
+            </li>
+          </ul>
         </div>
+      </div>
     </nav>
     <div class="container-div">
-        <form action="" method="POST" class="form-control">
+      <form action="" method="POST" class="form-control">
+        <p>Colabore com o Instituto Luiza Mell</p>
+        <p class="texto">
+          <img
+            class="rounded mx-auto d-block"
+            src="../imagens/luisaMell.jpg"
+            width="10%"
+            height="10%"
+          />
+        </p>
 
-            <p>Colabore com o Instituto Luiza Mell</p>
-            <p class="texto">
-                <img class="rounded mx-auto d-block" src="../imagens/luisaMell.jpg" width="10%" height="10%">
-            </p>
+        <input type="hidden" name="idusuario" value="" id="idusuario" />
+        <input
+          type="hidden"
+          name="idinstituicao"
+          value="6"
+          id="idinstituicao"
+        />
 
-            <input type="hidden" name="idusuario" value="" id="idusuario">
-            <input type="hidden" name="idinstituicao" value="6" id="idinstituicao">
-
-            <?php
+        <?php
             include("../bd/conexao.php");
             $sql="SELECT nome, cnpj, cidade, estado, descricao FROM instituicao WHERE idinstituicao = 6";
             $res = mysqli_query($con, $sql);
@@ -55,67 +81,98 @@
             while($sql = mysqli_fetch_array($res))
             {
             ?>
-                <p class="texto"><label for="nome"><?php echo $sql['nome'];?></label></p>
-                <p class="texto"><label for="cnpj"><?php echo $sql['cnpj'];?></label></p>
-                <p class="texto"><label for="cidade"><?php echo $sql['cidade'];?></label></p>
-                <p class="texto"><label for="estado"><?php echo $sql['estado'];?></label></p>
-                <p class="texto"><label for="estado"><?php echo $sql['descricao'];?></label></p>
-            <?php
+        <ul class="list-inline">
+          <p class="list-inline-item"><label for="nome"><?php echo $sql['nome'];?></label></p>
+          <p class="list-inline-item"><label for="cnpj"><?php echo $sql['cnpj'];?></label></p>
+          <p class="list-inline-item"><label for="cidade"><?php echo $sql['cidade'];?></label></p>
+          <p class="list-inline-item"><label for="estado"><?php echo $sql['estado'];?></label></p>
+          <p class="lead"><label for="estado"><?php echo $sql['descricao'];?></label></p>
+        </ul>
+        <?php
             }
             ?>
-                <?php mysqli_close($con);?>
+        <?php mysqli_close($con);?>
 
-                <p class="texto"><label for="valor">Valor da doação:</label></p>
-                <input class="form-control" type="number" name="valor" id="valor" required=" " autofocus=" " />          
-            
+        <p class="texto"><label for="valor">Valor da doação:</label></p>
+        <input
+          class="form-control"
+          type="number"
+          name="valor"
+          id="valor"
+          required=" "
+          autofocus=" "
+        />
 
-                <p class="texto "><label for="email ">Email:</label></p>
-                <input class="form-control " name="email " type="email " id="email" required=" " autofocus=" " />
+        <p class="texto"><label for="email ">Email:</label></p>
+        <input
+          class="form-control"
+          name="email "
+          type="email "
+          id="email"
+          required=" "
+          autofocus=" "
+        />
 
-                <p class="texto"><label for="senha">Senha:</label></p>
-                <input type="password" name="senha" id="senha" placeholder="Senha" class="form-control" minlength="8" autocomplete="off" required="" autofocus="">
-                <button type="button" id="btnCad" class="btn btn-outline-dark">Buscar cadastro</button><br />
-                <button type="button" id="btnEnviar" class="btn btn-dark my-4">Doar</button><br />
-        </form>
+        <p class="texto"><label for="senha">Senha:</label></p>
+        <input
+          type="password"
+          name="senha"
+          id="senha"
+          placeholder="Senha"
+          class="form-control"
+          minlength="8"
+          autocomplete="off"
+          required=""
+          autofocus=""
+        />
+        <button type="button" id="btnCad" class="btn btn-outline-dark">
+          Buscar cadastro</button
+        ><br />
+        <button type="button" id="btnEnviar" class="btn btn-dark my-4">
+          Doar</button
+        ><br />
+      </form>
     </div>
     <script>
-        $("#btnCad").click(function() {
-            $.post(
-                "../bd/buscarcad.php", {
-                    email: $("#email").val(),
-                    senha: $("#senha").val(),
-                },
-                function(data) {
-                    if (data.resp == false) {
-                        bootbox.alert("Cadastro não encontrado");
-                    } else {
-                        $("#idusuario").val(data.idusuario);
-                        bootbox.alert("Cadastro encontrado");
+      $("#btnCad").click(function () {
+        $.post(
+          "../bd/buscarcad.php",
+          {
+            email: $("#email").val(),
+            senha: $("#senha").val(),
+          },
+          function (data) {
+            if (data.resp == false) {
+              bootbox.alert("Cadastro não encontrado");
+            } else {
+              $("#idusuario").val(data.idusuario);
+              bootbox.alert("Cadastro encontrado");
+            }
+          },
+          "JSON"
+        );
+      });
+      $("#btnEnviar").click(function () {
+        $.post(
+          "../bd/doacao.php",
+          {
+            valor: $("#valor").val(),
+            idusuario: $("#idusuario").val(),
+            idinstituicao: $("#idinstituicao").val(),
+          },
+          function (data) {
+            if (data.resp == false) {
+              console.log("Cadastro não encontrado");
 
-                    }
-                },
-                "JSON")
-        }); 
-        $("#btnEnviar").click(function() {
-            $.post(
-                "../bd/doacao.php", {
-                    valor: $("#valor").val(),
-                    idusuario: $("#idusuario").val(),
-                    idinstituicao: $("#idinstituicao").val(),
-                },
-                function(data) {
-                    if (data.resp == false) {
-                        console.log("Cadastro não encontrado");
+              bootbox.alert(`Ocorreu um erro:"${data.msg}"`);
+            } else {
+              bootbox.alert("Cadastro realizado");
+            }
+          },
 
-                        bootbox.alert(`Ocorreu um erro:"${data.msg}"`);
-                    } else {
-                        bootbox.alert("Cadastro realizado"); 
-                    }
-                },
-
-                "JSON")
-        });
+          "JSON"
+        );
+      });
     </script>
-</body>
-
+  </body>
 </html>
--- pages/lar-vicentino.php
+++ pages/lar-vicentino.php
@@ -6,14 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/estilopaginas.css">
 
     <!-- JavaScript  -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js">
     </script>
@@ -24,8 +27,9 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand mx-4" href="#">Doar.com</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      </button>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
@@ -55,37 +59,40 @@
             while($sql = mysqli_fetch_array($res))
             {
             ?>
-                <p class="texto"><label for="nome"><?php echo $sql['nome'];?></label></p>
-                <p class="texto"><label for="cnpj"><?php echo $sql['cnpj'];?></label></p>
-                <p class="texto"><label for="cidade"><?php echo $sql['cidade'];?></label></p>
-                <p class="texto"><label for="estado"><?php echo $sql['estado'];?></label></p>
-                <p class="texto"><label for="estado"><?php echo $sql['descricao'];?></label></p>
+            <ul class="list-inline">
+                <p class="list-inline-item"><label for="nome"><?php echo $sql['nome'];?></label></p>
+                <p class="list-inline-item"><label for="cnpj"><?php echo $sql['cnpj'];?></label></p>
+                <p class="list-inline-item"><label for="cidade"><?php echo $sql['cidade'];?></label></p>
+                <p class="list-inline-item"><label for="estado"><?php echo $sql['estado'];?></label></p>
+                <p class=" lead"><label for="estado"><?php echo $sql['descricao'];?></label></p>
+            </ul>
             <?php
             }
             ?>
-                <?php mysqli_close($con);?>
+            <?php mysqli_close($con);?>
 
-                <p class="texto"><label for="valor">Valor da doação:</label></p>
-                <input class="form-control" type="number" name="valor" id="valor" required=" " autofocus=" " />          
-            
+            <p class="texto"><label for="valor">Valor da doação:</label></p>
+            <input class="form-control" type="number" name="valor" id="valor" required=" " autofocus=" " />
 
-                <p class="texto "><label for="email ">Email:</label></p>
-                <input class="form-control " name="email " type="email " id="email" required=" " autofocus=" " />
 
-                <p class="texto"><label for="senha">Senha:</label></p>
-                <input type="password" name="senha" id="senha" placeholder="Senha" class="form-control" minlength="8" autocomplete="off" required="" autofocus="">
-                <button type="button" id="btnCad" class="btn btn-outline-dark">Buscar cadastro</button><br />
-                <button type="button" id="btnEnviar" class="btn btn-dark my-4">Doar</button><br />
+            <p class="texto "><label for="email ">Email:</label></p>
+            <input class="form-control " name="email " type="email " id="email" required=" " autofocus=" " />
+
+            <p class="texto"><label for="senha">Senha:</label></p>
+            <input type="password" name="senha" id="senha" placeholder="Senha" class="form-control" minlength="8"
+                autocomplete="off" required="" autofocus="">
+            <button type="button" id="btnCad" class="btn btn-outline-dark">Buscar cadastro</button><br />
+            <button type="button" id="btnEnviar" class="btn btn-dark my-4">Doar</button><br />
         </form>
     </div>
     <script>
-        $("#btnCad").click(function() {
+        $("#btnCad").click(function () {
             $.post(
                 "../bd/buscarcad.php", {
                     email: $("#email").val(),
                     senha: $("#senha").val(),
                 },
-                function(data) {
+                function (data) {
                     if (data.resp == false) {
                         bootbox.alert("Cadastro não encontrado");
                     } else {
@@ -95,21 +102,21 @@
                     }
                 },
                 "JSON")
-        }); 
-        $("#btnEnviar").click(function() {
+        });
+        $("#btnEnviar").click(function () {
             $.post(
                 "../bd/doacao.php", {
                     valor: $("#valor").val(),
                     idusuario: $("#idusuario").val(),
                     idinstituicao: $("#idinstituicao").val(),
                 },
-                function(data) {
+                function (data) {
                     if (data.resp == false) {
                         console.log("Cadastro não encontrado");
 
                         bootbox.alert(`Ocorreu um erro:"${data.msg}"`);
                     } else {
-                        bootbox.alert("Cadastro realizado"); 
+                        bootbox.alert("Cadastro realizado");
                     }
                 },
 
